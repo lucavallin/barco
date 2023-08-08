627a0031d74e00360d5b5597ce15468c18aba65b
Christopher Wellons <wellons@nullprogram.com>
2023-08-08 19:06:21 -0400

    Do not pass an empty argv[1] by default
    This also fixes the execve failure log, which had been passed the argv
    array instead of the argument string. The changes also avoids passing
    the newly-introduced NULL.

--- src/container.c
+++ src/container.c
@@ -44,7 +44,9 @@ int container_start(void *arg) {
   // argv must be NULL terminated
   char *argv[] = {config->cmd, config->arg, NULL};
   if (execve(config->cmd, argv, NULL)) {
-    log_error("failed to execve '%s %s': %m", config->cmd, argv);
+    log_error("failed to execve '%s%s%s': %m", config->cmd,
+              config->arg == NULL ? "" : " ",
+              config->arg == NULL ? "" : config->arg);
     return -1;
   }
   log_debug("container started...");
